787295fce534b56405fe43ea4dbaba5e771ab4ef
sumido <sumidoemmanuel13@gmail.com>
2025-11-18 22:25:03 +0800

    day 5 or 6

--- demonlist.js
+++ demonlist.js
@@ -1,6 +1,3 @@
-//Player data
-const 
-  As1nus = '<img class="recordpfp" src="images/as1nus.png" alt="Competitor 1"> As1nus'
 
 
 //demonlist data
@@ -15,7 +12,8 @@ const demonlist = [//DATA
     <span class="tags"> Fast paced </span>
     `, 
     videolink:"https://drive.google.com/file/d/1i2JYamEOTHlpftOvk68kjK8ZTO2oHFxn/preview",
-    record1:`${As1nus} - First Victor`,
+    record1:`As1nus`, record1pic:"as1nus", victor1link:"https://www.youtube.com/channel/UCV8Z4f8lXoO6r0kLhH8cX9A",
+    record2:"Coldaf", record2pic:"coldaf",
   },
   {//NINE CIRCLES
     name: "Nine Circles", 
@@ -114,14 +112,25 @@ function information(demon, i) {
     </div>
 
     <div class="records">
-      <div class="record1 eachrecord"> ${demon.record1 ?? "No Record Yet"} </div>
-      <div class="record2 eachrecord"> ${demon.record2 ?? ""} </div>
-      <div class="record3 eachrecord"> ${demon.record3 ?? ""} </div>
+      <div class="record1 eachrecord"> <img class="recordpfp" id="${demon.record1pic ?? "norecordpic"}" src="images/${demon.record1pic}.png"> ${demon.record1 ?? "No Record Yet"} - First Victor <a href="${demon.victor1link ?? ""}" style="cursor: pointer;"> <img  style="float: right;" class="recordpfp" src="${demon.victor1link ? "images/greyyticon.png" : "images/nogreyyticon.png"}"> </a> </div>
+      <div class="record2 eachrecord" id="2recordplayer"> <img class="recordpfp" id="${demon.record2pic ?? "norecordpic2"}" src="images/${demon.record2pic}.png"> ${demon.record2 ?? "No Record Yet"} <a href="${demon.victor2link ?? ""}" style="cursor: pointer;"> <img style="float: right;" class="recordpfp" src="${demon.victor2link ? "images/greyyticon.png" : "images/nogreyyticon.png"}"> </a> </div>
+      <div class="record3 eachrecord" id="3recordplayer"> <img class="recordpfp" id="${demon.record3pic ?? "norecordpic3"}" src="images/${demon.record3pic}.png"> ${demon.record3 ?? "No Record Yet"} <a href="${demon.victor3link ?? ""}" style="cursor: pointer;"> <img style="float: right;" class="recordpfp" src="${demon.victor3link ? "images/greyyticon.png" : "images/nogreyyticon.png"}"> </a> </div>
+      ${demon.record4 ?? ""}
     </div>
 
     <div class="other">
       <div class="othercontent"> ${getDemonPoints(i + 1, demon.difficulty)} points</div>
     </div>
   `;
+
+  
+    if (document.getElementById("norecordpic2")) {
+      document.getElementById("2recordplayer").style.display = "none";
+    }
+
+    if (document.getElementById("norecordpic3")) {
+      document.getElementById("3recordplayer").style.display = "none";
+    }
+
 }
 
